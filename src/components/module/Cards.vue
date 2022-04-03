fc9c7565eb6f959204c7dcfc7749b42b056179fc
VsKendo <1059227871@qq.com>
2022-04-03 14:57:12 +0800

    让友链更好看

--- src/components/module/Cards.vue
+++ src/components/module/Cards.vue
@@ -1,18 +1,17 @@
 <template>
-  <a-space align="center"  :size="40" class="link-navigation">
+  <a-space align="center" :size="40" class="link-navigation">
     <div v-for="i in 3">
       <div v-if="index+i-1<friends.length" class="card">
         <a :href="friends[index+i-1].link" target="_blank">
-          <img class="ava" :data-src="friends[index+i-1].img" :src="friends[index+i-1].img" data-loaded="true"
+          <img class="card-ava" :data-src="friends[index+i-1].img" :src="friends[index+i-1].img" data-loaded="true"
                :alt="index+i-1">
           <div class="card-header">
             <div>{{ friends[index + i - 1].name }}</div>
-            <div class="info">{{ friends[index + i - 1].desc }}</div>
+            <div class="card-info">{{ friends[index + i - 1].desc }}</div>
           </div>
         </a>
       </div>
-      <div v-else style="width: 300px;" />
-
+      <div v-else style="width: 300px;"/>
     </div>
   </a-space>
 </template>
@@ -50,12 +49,12 @@ export default class Cards extends Vue {
 
 .card {
   width: 300px;
-  font-size: 1rem;
-  padding: 10px 15px 10px;
-  border-radius: 4px;
+  min-width: 300px;
+  height: 200px;
+  min-height: 200px;
+  padding: 10px 20px 10px;
   transition-duration: .15s;
   margin-bottom: 1rem;
-  display: flex;
   line-height: 2;
   border: 3px solid #e8e8e8;
 }
@@ -65,20 +64,33 @@ export default class Cards extends Vue {
   box-shadow: 0 2px 6px 0 rgba(0, 0, 0, .12), 0 0 6px 0 rgba(0, 0, 0, .04);
 }
 
-.card .card-header {
-  font-style: italic;
-  overflow: hidden;
-  width: 236px;
-  line-height: 2;
-}
-
-.ava {
-  width: 3rem !important;
-  height: 3rem !important;
-  margin: 0 !important;
-  margin-right: 1em !important;
+.card-ava {
+  width: 80px !important;
+  min-width: 80px !important;
+  height: 80px !important;
+  margin-bottom: 10px !important;
   border-radius: 4px;
 }
+
+.card-header {
+  margin-bottom: 0 !important;
+  width: 250px;
+  color: #3c4248;
+  font-family: Dosis, Open Sans, -apple-system, Arial, Microsoft Yahei, Microsoft Jhenghei, sans-serif;
+  font-weight: 500 !important;
+  font-size: 22px;
+  letter-spacing: .5px;
+  line-height: 1.5;
+  margin-top: 0;
+}
+
+.card-info {
+  color: inherit !important;
+  opacity: .7;
+  font-size: 16px;
+  margin-top: 5px;
+}
+
 
 @media screen and (max-width: $--screen-sm-min) {
   .link-navigation {
